4c61c435c1e78eb938f1fe373d57f14474ae9bfe
furkanBilici <fbilici258@gmail.com>
2024-12-31 14:32:42 +0300

    Builder eklendi

---
+++ Builder/Business/ProductBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Builder.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder.Business
+{
+    public abstract class ProductBuilder
+    {
+        public abstract ProductViewModel GetProductData();//daha farklı değerler döndürebilir
+        public abstract void ApplyDiscount(); 
+    }
+}
